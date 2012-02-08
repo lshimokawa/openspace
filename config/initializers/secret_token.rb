b8e40175160ba03d29614cc1936b384a975bf490
Lennon Shimokawa <lshimokawa@gmail.com>
2012-02-07 19:25:16 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openspace::Application.config.secret_token = '2664e754b430b704752e3fa9715ef9d34e069cdb84d7d2027f3aed2e10871c8c3020aeb310830424f4db9f34dfacb816ead92b335d45e08807e10da6fe1ac953'
