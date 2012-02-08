52a2d1b5c70710e8ec3ee3ce6b7c41676e63ba82
Lennon Shimokawa <lshimokawa@gmail.com>
2012-02-08 18:42:17 -0500

    scaffold de session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,83 @@
+class SessionsController < ApplicationController
+  # GET /sessions
+  # GET /sessions.json
+  def index
+    @sessions = Session.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @sessions }
+    end
+  end
+
+  # GET /sessions/1
+  # GET /sessions/1.json
+  def show
+    @session = Session.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @session }
+    end
+  end
+
+  # GET /sessions/new
+  # GET /sessions/new.json
+  def new
+    @session = Session.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @session }
+    end
+  end
+
+  # GET /sessions/1/edit
+  def edit
+    @session = Session.find(params[:id])
+  end
+
+  # POST /sessions
+  # POST /sessions.json
+  def create
+    @session = Session.new(params[:session])
+
+    respond_to do |format|
+      if @session.save
+        format.html { redirect_to @session, notice: 'Session was successfully created.' }
+        format.json { render json: @session, status: :created, location: @session }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @session.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /sessions/1
+  # PUT /sessions/1.json
+  def update
+    @session = Session.find(params[:id])
+
+    respond_to do |format|
+      if @session.update_attributes(params[:session])
+        format.html { redirect_to @session, notice: 'Session was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @session.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sessions/1
+  # DELETE /sessions/1.json
+  def destroy
+    @session = Session.find(params[:id])
+    @session.destroy
+
+    respond_to do |format|
+      format.html { redirect_to sessions_url }
+      format.json { head :no_content }
+    end
+  end
+end
