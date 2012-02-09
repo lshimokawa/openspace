75f10ec745f9fd1b16d276851cf5b8ef3f3b3774
Lennon Shimokawa <lshimokawa@gmail.com>
2012-02-08 19:19:40 -0500

    authentication controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120208234122) do
+ActiveRecord::Schema.define(:version => 20120209001430) do
 
   create_table "sessions", :force => true do |t|
     t.string   "title"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
