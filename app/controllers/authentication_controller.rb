be195d877e49c7506e98be51fd47567c11290e58
Lennon Shimokawa <lshimokawa@gmail.com>
2012-02-22 13:01:40 -0500

    redirecting to root

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to :back
+    redirect_to root_url
   end
 
   def destroy
