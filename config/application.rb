fc749448af7de5b156090364ea1d4aee1be004ef
Lennon Shimokawa <lshimokawa@gmail.com>
2012-02-22 11:49:41 -0500

    configurando default locale para heroku

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Openspace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
+    
+    #Adding the below makes it work as expected at heroku
+    config.i18n.locale = :es
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
