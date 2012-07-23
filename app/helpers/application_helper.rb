071fffe6ef6b9dee0ba1ebdbc9f5fc15e09833a5
Lennon Shimokawa <lshimokawa@gmail.com>
2012-07-23 10:16:41 -0500

    users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,33 @@
 module ApplicationHelper
   
+  def link_to_home
+    link_to "Inicio", root_path, 'data-icon'=> 'home', 'data-direction'=> 'reverse'
+  end
+  
+  def link_to_authentication
+    if current_user
+      link_to "Logout", signout_path, 'data-role'=> 'button', 'data-icon'=>'twitter', 'class'=>'ui-btn-right'
+    else
+      link_to "Signin", "/auth/twitter", 'data-role'=>'button', 'data-icon'=>'twitter', 'class'=>'ui-btn-right', :rel => 'external'
+    end
+  end
+  
+  def link_to_event
+    link_to "Evento", menu_event_path(current_event), 'data-icon'=> 'back', 'data-direction'=> 'reverse'
+  end
+  
+  def link_to_sessions
+    link_to "Sesiones", event_sessions_path(current_event), 'data-icon'=> 'back', 'data-direction'=> 'reverse'
+  end
+  
+  def link_to_agenda
+    link_to "Agenda", event_agenda_index_path(current_event), 'data-icon'=> 'back', 'data-direction'=> 'reverse'
+  end
+  
+  def link_to_users
+    link_to "Regresar", users_path, "data-icon"=>"back", 'data-direction'=> 'reverse'
+  end
+  
   #TODO hay una mejor forma de hacer esto?
   def href_link(url)
     "<a href='#{url}' rel='external'>#{url}</a>".html_safe
