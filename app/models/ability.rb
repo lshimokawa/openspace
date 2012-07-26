5c804696422e501dfcf149a88826fbb8c07336ac
Lennon Shimokawa <lshimokawa@gmail.com>
2012-07-26 09:00:30 -0500

    model documentation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,6 +1,20 @@
+# Configuración de CanCan, aquí está la autorización
 class Ability
   include CanCan::Ability
 
+  # The first argument to `can` is the action you are giving the user permission to do.
+  # If you pass :manage it will apply to every action. Other common actions here are
+  # :read, :create, :update and :destroy.
+  #
+  # The second argument is the resource the user can perform the action on. If you pass
+  # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
+  #
+  # The third argument is an optional hash of conditions to further filter the objects.
+  # For example, here the user can only update published articles.
+  #
+  #   can :update, Article, :published => true
+  #
+  # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.role == 'admin'
@@ -11,19 +25,5 @@ class Ability
     else
       can :read, :all
     end
-    
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
