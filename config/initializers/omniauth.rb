6f2c743872f800594d2d5bea6ab508a1985530cc
Karen Da Cruz <me@karendacruz.com>
2012-07-25 19:33:56 -0500

    Agregando Facebook Sign in

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'YTy0vUAW14iGHJyybrIFA', 'QO4ogMO2pfA2WjCVqOq77wBqnog7r2TGxJ8GNNxcEeM'
+  provider :facebook, '343775789038073', '0d96c843e0984bdefcc728106fb37e41', :scope => 'email', :display => 'popup'
 end
