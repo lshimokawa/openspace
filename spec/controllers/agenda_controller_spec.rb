50391d5f8e12a7c11f472fd6c0d6099586fe5abb
Lennon Shimokawa <lshimokawa@gmail.com>
2012-08-07 12:10:48 -0500

    clean specs

--- spec/controllers/agenda_controller_spec.rb
+++ spec/controllers/agenda_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe AgendaController do
   render_views
   
-  it "show the agenda of the event" do
+  it "shows the agenda of the event" do
     agenda = Factory(:agenda)
     get :index, :event_id => agenda.event.id
     assigns(:agenda).should eq([agenda])
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
 describe ApplicationController do
-
-  it "should return current user in session" do 
-    pending "como pruebo metodos privados?"
-  end
   
-  it "should return current event in session" do 
-    pending "como pruebo metodos privados?"
-  end
-
 end
